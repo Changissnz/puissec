3599c65cedf6ce49450eb5a056a69ff18f4552c0
Richard Pham <phamrichard45@gmail.com>
2024-05-19 22:03:36 -0400

    add new file for later date of completion

--- tdir.py
+++ tdir.py
@@ -3,3 +3,11 @@ Was supposed to be named Traversal Directing [Wang Fong Qhong].
 By and through the connection. 
 By,through, and for the connection.
 """
+
+class TDir:
+
+    def __init__(self):
+        return -1
+
+    def __next__(self):
+        return -1 
