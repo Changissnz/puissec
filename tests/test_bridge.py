caf9f33bfa28527c8690dac2e4e150d0035d6398
Richard Pham <phamrichard45@gmail.com>
2024-08-15 08:57:15 -0400

    add another bridge test on: approximate HypStruct

--- tests/test_bridge.py
+++ tests/test_bridge.py
@@ -1,6 +1,7 @@
 from secnet import * 
 import unittest
 
+# TODO: unused,delete?
 def CBridge_case_X(guess_index=3):
 
 
@@ -42,6 +43,42 @@ def CBridge_case_X(guess_index=3):
     cb = CBridge(c,ir,hs,ssih=2,cidn=1,\
         batch_size=2000,verbose=True)
     return cb 
+###############################################
+
+def CBridge_case_ApproximateHypStruct_num0():
+
+    random.seed(2004)
+    np.random.seed(2004)
+
+    ss = SecSeq_sample_4(num_secs=1,\
+            singleton_range=DEFAULT_SINGLETON_RANGE,\
+            num_conn=1,min_components=1,max_nconn_ratio = 0.3,\
+            drange_max=1)
+
+    sc = ss[0] 
+    print(sc[0]) 
+
+    bound = [0.,1.]
+    irc = IsoRingedChain(sc,bound,random,71)
+    for x in irc.irl:
+        x.explode_contents()
+    q = irc[0]
+
+    # [10, 6, 3, 2, 8, 9]
+    seci = 2
+    bound_length = np.array([0.4,0.8,0.5])
+    location_ratio = np.array([0.1,0.2,0.6])
+    hop_size = 5
+    hs = one_approximate_HypStruct_for_IsoRing(q,seci,\
+        bound_length,location_ratio,hop_size)
+
+
+    # run a <CRridge> cracking session
+    cr = Crackling(cidn=2,cvsz=200)
+    cr.load_HypStruct(hs) 
+
+    cb  = CBridge(cr,q,hs,ssih=5,cidn=12,batch_size=100,verbose=True)
+    return cb 
 
 ### lone file test 
 """
@@ -77,6 +114,44 @@ class CBridgeClass(unittest.TestCase):
                 stat = type(qc) != type(None)
                 stat = stat and (i < 100)
                 i += 1 
+
+    def test__CBridge__next__ApproximateHypStruct__num0(self):
+
+        cb = CBridge_case_ApproximateHypStruct_num0() 
+        cr = cb.crackling 
+        q = cb.isoring 
+        i = 0 
+        il = 300
+        while i < il and not cr.astat: 
+            print("i: ",i)
+            next(cb)
+            i += 1
+
+        assert i == 8
+
+        l = len(cr.flagged_pts)
+
+        print("STAT")
+        print("- c : ",cr.cstat)
+        print("- i : ",cr.istat)
+        print("- a: ", cr.astat)
+
+        print("FLAGGED POINTS")
+        print(l)
+
+        assert not cr.cstat and not cr.istat
+        assert cr.astat 
+        assert l == 4
+        print(cr.flagged_pts)
+
+        assert len(cr.cracked_dict) == 1 
+        sx = '0.00515,0.62506,0.15195'
+        vx = cr.cracked_dict[sx]
+        assert vx == q.sec.opm[sx]
+
+        qr1 = matrix_methods.string_to_vector(sx,float)
+        qr1 = np.round(qr1,5) 
+        assert (q.sec.seq == qr1).all() 
 
     """
     demonstration w/ the perfect hypothesis
